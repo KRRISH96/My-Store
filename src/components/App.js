aa786a5ab0aeb8fbcfefb726ef96cdc2a1d75b17
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2018-06-19 22:48:40 +0530

    Initial Setup

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>My-Store APP</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
