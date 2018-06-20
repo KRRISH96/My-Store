a3dacf8f740a78af302115d899e920698b6b9e45
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2018-06-20 06:54:38 +0530

    Landing Page Initial Data

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,38 @@
 import React, { Component } from 'react';
-
+import { GetAllProducts } from '../moltin.js'
 
 class App extends Component {
+  state = {
+    products: [],
+    loading: true
+  };
+
+  async componentDidMount() {
+    const AllProducts = await GetAllProducts
+    this.setState({
+      products: [...AllProducts.data],
+      loading: false
+    })
+    console.log(AllProducts)
+  };
+
   render() {
-    return (
-      <div className="App">
-        <h1>My-Store APP</h1>
-      </div>
-    );
+    const { products, files, loading } = this.state;
+      console.log(products, 'state')
+      console.log(files)
+    return loading === true
+      ? <h1>Loading</h1>
+      : <div style={{display: 'flex', justifyContent:'space-around'}}>
+          {products.map(product => (
+            <div key={product.id}>
+              <img src={product.relationships.main_image.data.id} alt={product.name}></img>
+              <h1>{product.name}</h1>
+              <p>{product.description}</p>
+              <p>{product.meta.display_price.with_tax.formatted}</p>
+              <button>CheckOut</button>
+            </div>
+          ))}
+        </div>
   }
 }
 
---
+++ src/moltin.js
@@ -0,0 +1,12 @@
+import { gateway as MoltinGateway } from '@moltin/sdk'
+// const MoltinGateway = require("@moltin/sdk").gateway;
+
+const Moltin = MoltinGateway({
+  client_id: 'JVnNlxLA8lEh7hMb7pxBjLu6LInKYMhQ33pNT31cMJ',
+})
+
+export const GetAllProducts = Moltin.Products.With('files, main_images').All().then(products => {
+  return products
+});
+
+export const GetProduct = (Id) => Moltin.Products.Get(Id)
