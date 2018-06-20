fb656948f88ecbcf0bdb2c4083d48654db98e665
Sai Krishna Prasad Kandula <kskprasad96@gmail.com>
2018-06-20 19:31:00 +0530

    Checkout page statically functional

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -9,9 +9,10 @@ class Checkout extends Component {
    name: 'Test',
   first_name:'Test',
   last_name:'Function',
+  company_name: 'personal',
   line_1:'#123, street5',
   line_2: 'test state, country',
-  city:'',
+  city:'Hitech',
   postcode:'123456',
   county: 'ABC',
   country: 'US',
@@ -21,10 +22,10 @@ class Checkout extends Component {
 
 
   async componentDidMount() {
-   const {reference, email,name,first_name, last_name, line_1, line_2, city, postcode, county, country} = this.state
+   const {reference, email,name,first_name, last_name, company_name, line_1, line_2, city, postcode, county, country} = this.state
    const customer = { email, name }
    const billing = {
-     first_name, last_name, line_1, line_2, city, postcode,county,  country
+     first_name, last_name, company_name,line_1, line_2, city, postcode,county,  country
    }
    const checkoutResults = await Checking(reference,customer,billing);
    this.setState({
@@ -35,12 +36,35 @@ class Checkout extends Component {
 
   render() {
     const { checkoutResults,loading } = this.state
-    console.log(checkoutResults,'im at render')
     return loading === true
       ? <h1>Loading...</h1>
-      : <div>
+      : <div style={{display:'flex', flexDirection: 'column',alignItems:'center'}}>
           <h1>Checkout</h1>
-          <p>Name:{checkoutResults.data.customer.name}</p>
+          <h3>Name:{checkoutResults.data.customer.name}</h3>
+          <h3>Email: {checkoutResults.data.customer.email}</h3>
+          <div style={{display:'flex'}}>
+          <div>
+            <h2>Billing Address</h2>
+            <ul>
+              {Object.keys(checkoutResults.data.billing_address).map((entry,index) => (
+                <li key={index}>
+                  <strong>{entry}</strong> -----> {Object.values(checkoutResults.data.billing_address)[index]}
+                </li>
+              ))}
+            </ul>
+            </div>
+            <div>
+            <h2>Shipping Address</h2>
+            <ul>
+              {Object.keys(checkoutResults.data.shipping_address).map((entry,index) => (
+                <li key={index}>
+                  <strong>{entry}</strong> -----> {Object.values(checkoutResults.data.shipping_address)[index]}
+                </li>
+              ))}
+            </ul>
+            </div>
+          </div>
+          <h2>Total Price: {checkoutResults.data.meta.display_price.with_tax.formatted}</h2>
           <Link to='/cart'>Go Back</Link>
         </div>
   }
